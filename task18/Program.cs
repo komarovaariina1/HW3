6c9be9996b6c908e5c91e42d2124f8414da03781
Arina <komarovaariina@mail.ru>
2022-09-17 14:46:22 +0300

    task18

---
+++ task18/Program.cs
@@ -0,0 +1,18 @@
+﻿
+
+
+Console.WriteLine("Введите номер четверти: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc)
+
+{
+    if (xc == 1) return "x > 0, y > 0";
+    if (xc == 1) return "x < 0, y > 0";
+    if (xc == 1) return "x < 0, y < 0";
+    if (xc == 1) return "x > 0, y < 0";
+    return "Введите число от 1 до 4";
+}
+    
+    string result = Quarter(x);
+    Console.WriteLine(result);
