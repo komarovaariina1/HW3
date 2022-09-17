d89756f8bc8703a7320a0dd42cd779a9828c0e70
Arina <komarovaariina@mail.ru>
2022-09-17 14:41:17 +0300

    task17

---
+++ task17/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход координаты точки (X и Y), причм X не равно 0 и Y не равно 0 и выдает номер четверти плоскости, в которой находится эта точка 
+
+Console.WriteLine("Введите координаты точки");
+Console.Write("X: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+string Quarter(int xc, int yc)
+{
+    if(xc > 0 && yc > 0) return "Первая четверть";
+    if(xc < 0 && yc > 0) return "Вторая четверть";
+    if(xc < 0 && yc < 0) return "Третья четверть";
+    if(xc > 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некоректные координаты";
+
+}
+
+string result = Quarter(x, y);
+Console.WriteLine(result);
