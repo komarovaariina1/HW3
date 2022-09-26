534424e2a05eba862eb1e7c01d79f9e484a81d40
Arina <komarovaariina@mail.ru>
2022-09-26 13:56:17 +0300

    task27

--- task32/Program.cs
+++ task32/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу замены элементов массива: положительные элементы заените на соответствующие отрицательные и наоборот.
+
+
